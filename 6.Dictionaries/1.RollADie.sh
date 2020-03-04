80340a08f59f581a469547a4d6400861a3933274
Vinayak Ushakola <vinayak.ushakola@gmail.com>
2020-03-04 16:28:12 +0530

    Added names

--- 6.Dictionaries/1.RollADie.sh
+++ 6.Dictionaries/1.RollADie.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 die=0
-declare -A dice
-while [[ ${dice["$die"]} -ne 10 ]]
+declare -a dice
+while [[ ${dice["$die"]} -ne 5 ]]
 do
    die=$(( (RANDOM%6)+1 ))
    dice["$die"]=$(( ${dice["$die"]}+1 ))
 done
 
-echo ${dice[@]}
-echo ${!dice[@]}
+echo "value: ${!dice[@]}"
+echo "count: ${dice[@]}"
