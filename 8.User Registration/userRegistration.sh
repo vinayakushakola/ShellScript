1d1cd5ec3fa7eea0c229000379982c66ff80123f
Vinayak Ushakola <vinayak.ushakola@gmail.com>
2020-03-04 09:53:21 +0530

    Fixed Error

--- 8.User Registration/userRegistration.sh
+++ 8.User Registration/userRegistration.sh
@@ -13,26 +13,26 @@ function check () {
 	namePattern="^[A-Z]{1}[a-z]{2,}$"
 	emailPattern="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
 	phonePattern="^([0-9]{1,3}[ ][1-9]{1}[0-9]{9})$"
-	passPattern="^[a-zA-Z0-9]{8,}$"+"^(?=.*[A-Z])[a-zA-Z]{8,}$"+"^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$"+"^((?=[^@|#|&|%|!|$]*[@|#|&|%|!|$][^@|#|&|%|!|$]*$)(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9@#&%!$]{8,})$"
-	if [ $name =~ $namePattern ]
+	passPattern="^[a-zA-Z0-9]{8,}$"
+	if [[ $name =~ $namePattern ]]
 	then
-		echo $name is Successfully Registered
+		echo -e "\n"$name is Successfully Registered
 	else
 		echo Invalid
 	fi
-	if [ $phone =~ $phonePattern ]
+	if [[ $phone =~ $phonePattern ]]
         then
                 echo Phone number is Successfully Registered
         else
                 echo Invalid
 	fi
-	if [ $email =~ $emailPattern ]
+	if [[ $email =~ $emailPattern ]]
         then
                 echo $email is Successfully Registered
         else
                 echo Invalid
 	fi
-	if [ $password =~ $passPattern ]
+	if [[ $password =~ $passPattern ]]
         then
                 echo $password is Successfully Registered
         else
